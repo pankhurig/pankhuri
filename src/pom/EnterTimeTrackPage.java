e12716fb8efef7c4515259bba3cf5c8ca4d1a11a
Aravind <Aravind@Aravind-HP>
2017-02-28 18:39:19 +0530

    update

---
+++ src/pom/EnterTimeTrackPage.java
@@ -0,0 +1,22 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import generic.BasePage;
+
+public class EnterTimeTrackPage extends BasePage {
+
+	@FindBy(id="logoutLink")
+	private WebElement logoutLink;
+	
+	public EnterTimeTrackPage(WebDriver driver) {
+		super(driver);		
+	}
+	
+	public void clickLogout(){
+		logoutLink.click();
+	}
+
+}
